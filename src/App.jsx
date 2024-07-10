27af01b473378f7b43e83c694aefad1cae41fcc1
Rajan kumar <133194869+rajan0323@users.noreply.github.com>
2024-07-10 12:17:40 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { BrowserRouter } from "react-router-dom";
 
 import { About, Contact, Experience, Feedbacks, Hero, Navbar, Tech, Works, StarsCanvas } from "./components";
+import Education from "./components/Education";
 
 const App = () => {
   return (
@@ -12,6 +13,7 @@ const App = () => {
         </div>
         <About />
         <Experience />
+        <Education /> {/* Add Education component here */}
         <Tech />
         <Works />
         <Feedbacks />
